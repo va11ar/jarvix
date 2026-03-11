15263269ea078c07bda54c52c4a9fdcff14bad1a
Mohamed ElSamahy <elsamahy.mohamed@gmail.com>
2026-03-11 14:42:37 +0200

    Enhanced the resize between library and agents list, pressing X to exit shows the exit confirmation dialog

--- renderer/renderer.js
+++ renderer/renderer.js
@@ -177,7 +177,7 @@ function wireEventListeners() {
   // Window controls
   document.getElementById('btn-minimize').addEventListener('click', () => window.api.minimizeWindow())
   document.getElementById('btn-maximize').addEventListener('click', () => window.api.maximizeWindow())
-  document.getElementById('btn-close').addEventListener('click', () => window.api.closeWindow())
+  document.getElementById('btn-close').addEventListener('click', handleFileMenuExit)
 
   // File menu (Menu button)
   setupFileMenu()
